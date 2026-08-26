bbb076b98c1699565aaebd73aa3cd8d30f50bdf7
nikkanap <nenaputo@up.edu.ph>
2026-08-26 15:25:59 +0800

    Create generate_prediction_csv.py

---
+++ ML/generate_prediction_csv.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+def generate_prediction_csv(
+  y_pred_proba,
+  idx,
+  csv_file_path,
+  attribute_name
+):
+  predictions_df = pd.DataFrame({f'{attribute_name}_{idx}' : y_pred_proba})
+
+  if idx > 0:
+      predictions_df = pd.read_csv(csv_file_path)
+      predictions_df[f'{attribute_name}_{b}'] = y_pred_proba
+  predictions_df.to_csv(csv_file_path, index=False)
