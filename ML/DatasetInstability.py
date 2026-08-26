c58c91b3ef09e52a5f2ab9dd89b9cb3794cf8117
nikkanap <nenaputo@up.edu.ph>
2026-08-26 16:04:49 +0800

    Update main instability files to accommodate changes

--- ML/DatasetInstability.py
+++ ML/DatasetInstability.py
@@ -1,35 +1,23 @@
-from datasets import load_dataset
-import pandas as pd
 import numpy as np
 
 from LR import LR_Model
 from RF import RF_Classifier
 from XGB import XGBoost_Model
 from MLP import MLP_Model
-from ML.LSVC import LinearSVC_Model
-from metrics import make_scatter, percentile, MAPE, top_k_jaccard, cii, pr_auc_sd, brier_score_sd
+from LSVC import LinearSVC_Model
+from ML.init_database import init_dataset
 
 from sklearn.model_selection import KFold
 from sklearn.preprocessing import StandardScaler
 
-# loading the dataset
-dataset = load_dataset("imodels/compas-recidivism")
-training_df = pd.DataFrame(dataset['train'])  # get the training data
-testing_df = pd.DataFrame(dataset['test'])    # get the testing data
+# init the dataset to train & test vars
+[X_train, y_train, X_test, y_test] = init_dataset()
 
-# adding permanent identifier ids for metrics
-training_df["defendant_id"] = range(len(training_df))
-testing_df["defendant_id"] = range(len(testing_df))
+# set default random seed
+random_seed = 42    
 
 # kfold init
-k_fold = KFold(n_splits=10, shuffle=True, random_state=42)
-# separate the training data into X_train and y_train
-X_train = training_df.drop(columns=['is_recid'])
-y_train = training_df['is_recid'].to_numpy()
-
-# same goes for the test data
-X_test = testing_df.drop(columns=['is_recid'])
-y_test = testing_df['is_recid'].to_numpy()
+k_fold = KFold(n_splits=10, shuffle=True, random_state=random_seed)
     
 for fold_id, (train_idx, test_idx) in enumerate(k_fold.split(X_train)):
     X_pool = X_train[train_idx]
@@ -38,11 +26,11 @@ for fold_id, (train_idx, test_idx) in enumerate(k_fold.split(X_train)):
     X_val = X_train[test_idx]
     y_val = y_train[test_idx]
 
-    # bootstrapping for 21 rows la anay
+    # bootstrapping for 10 rows la anay
     for b in range(10): 
-        print(f"ITERATION {b}" )
+        print(f"BOOTSTRAP #{b}" )
         
-            # make bootstrapped sample of training dataset
+        # make bootstrapped sample of training dataset
         n_samples = len(X_pool)
         boot_idx = np.random.choice(
             n_samples, 
@@ -52,32 +40,24 @@ for fold_id, (train_idx, test_idx) in enumerate(k_fold.split(X_train)):
         X_boot = X_pool.iloc[boot_idx]
         y_boot = y_pool[boot_idx]
         
-        data = [
-            X_boot, y_boot,
-            X_val, y_val,
-            X_test, y_test,
-            b
-        ]
+        X = [X_boot, X_val, X_test] 
+        y = [y_boot, y_val, y_test]
         
-        RF_Classifier(data)
-        XGBoost_Model(data)
+        # Train RF and XGBoost models
+        RF_Classifier(X, y, fold_id, random_seed, b)
+        XGBoost_Model(X, y, fold_id, random_seed, b)
         
         # fit the training data and transform the test data
         scaler = StandardScaler()
         X_boot_scaled = scaler.fit_transform(X_boot)
         X_val_transf = scaler.transform(X_val)
         X_test_transf = scaler.transform(X_test)
-
-        data_transf = [
-            X_boot_scaled, y_boot,
-            X_val_transf, y_val,
-            X_test_transf, y_test,
-            fold_id, b
-        ]
+        X_scaled = [X_boot_scaled, X_val_transf, X_test_transf]
         
-        LR_Model(data_transf)
-        MLP_Model(data_transf)
-        LinearSVC_Model(data_transf)
+        # Train LR, MLP, and LinearSVC models
+        LR_Model(X_scaled, y, fold_id, random_seed, b)
+        MLP_Model(X_scaled, y, fold_id, random_seed, b)
+        LinearSVC_Model(X_scaled, y, fold_id, random_seed, b)
         
 """
 # displaying as a scatter plot
--- ML/StochasticInstability.py
+++ ML/StochasticInstability.py
@@ -1,35 +1,19 @@
-from datasets import load_dataset
-import pandas as pd
-import numpy as np
-
 from LR import LR_Model
 from RF import RF_Classifier
 from XGB import XGBoost_Model
 from MLP import MLP_Model
-from ML.LSVC import LinearSVC_Model
-from metrics import make_scatter, percentile, MAPE, top_k_jaccard, cii, pr_auc_sd, brier_score_sd
+from LSVC import LinearSVC_Model
+from ML.init_database import init_dataset
 
+import xgboost as xgb
 from sklearn.model_selection import KFold
 from sklearn.preprocessing import StandardScaler
 
-# loading the dataset
-dataset = load_dataset("imodels/compas-recidivism")
-training_df = pd.DataFrame(dataset['train'])  # get the training data
-testing_df = pd.DataFrame(dataset['test'])    # get the testing data
-
-# adding permanent identifier ids for metrics
-training_df["defendant_id"] = range(len(training_df))
-testing_df["defendant_id"] = range(len(testing_df))
+# init the dataset to train & test vars
+[X_train, y_train, X_test, y_test] = init_dataset()
 
 # kfold init
 k_fold = KFold(n_splits=10, shuffle=True, random_state=42)
-# separate the training data into X_train and y_train
-X_train = training_df.drop(columns=['is_recid'])
-y_train = training_df['is_recid'].to_numpy()
-
-# same goes for the test data
-X_test = testing_df.drop(columns=['is_recid'])
-y_test = testing_df['is_recid'].to_numpy()
     
 for fold_id, (train_idx, test_idx) in enumerate(k_fold.split(X_train)):
     X_pool = X_train[train_idx]
@@ -38,46 +22,31 @@ for fold_id, (train_idx, test_idx) in enumerate(k_fold.split(X_train)):
     X_val = X_train[test_idx]
     y_val = y_train[test_idx]
 
-    # bootstrapping for 21 rows la anay
-    for b in range(10): 
-        print(f"ITERATION {b}" )
+    # random seed from 0 to 9
+    for random_seed in range(10): 
+        print(f"RANDOM_SEED #{random_seed}" )
+        X = [X_train, X_val, X_test] 
+        y = [y_train, y_val, y_test]
         
-            # make bootstrapped sample of training dataset
-        n_samples = len(X_pool)
-        boot_idx = np.random.choice(
-            n_samples, 
-            size=n_samples, 
-            replace=True
-        )
-        X_boot = X_pool.iloc[boot_idx]
-        y_boot = y_pool[boot_idx]
+        RF_Classifier(X, y, fold_id, random_seed)
         
-        data = [
-            X_boot, y_boot,
-            X_val, y_val,
-            X_test, y_test,
-            b
-        ]
+        # convert data for xgboost
+        xgb_train = xgb.DMatrix(X[0], y[0], enable_categorical=False)
+        xgb_val = xgb.DMatrix(X[1], y[1], enable_categorical=False)
+        xgb_test = xgb.DMatrix(X[2], y[2], enable_categorical=False)
+        X_xgb = [xgb_train, xgb_val, xgb_test]
+        XGBoost_Model(X_xgb, y, fold_id, random_seed)
         
-        RF_Classifier(data)
-        XGBoost_Model(data)
-        
-        # fit the training data and transform the test data
+        # fit training data and transform the test data
         scaler = StandardScaler()
-        X_boot_scaled = scaler.fit_transform(X_boot)
+        X_train_scaled = scaler.fit_transform(X_train)
         X_val_transf = scaler.transform(X_val)
         X_test_transf = scaler.transform(X_test)
-
-        data_transf = [
-            X_boot_scaled, y_boot,
-            X_val_transf, y_val,
-            X_test_transf, y_test,
-            fold_id, b
-        ]
+        X_scaled = [X_train_scaled, X_val_transf, X_test_transf]
         
-        LR_Model(data_transf)
-        MLP_Model(data_transf)
-        LinearSVC_Model(data_transf)
+        LR_Model(X_scaled, y, fold_id, random_seed)
+        MLP_Model(X_scaled, y, fold_id, random_seed)
+        LinearSVC_Model(X_scaled, y, fold_id, random_seed)
         
 """
 # displaying as a scatter plot
