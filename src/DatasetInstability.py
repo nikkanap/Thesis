edb0fda09d61064f354656e03351e294c2b7491f
nikkanap <nenaputo@up.edu.ph>
2026-09-13 13:59:39 +0800

    Add comments and minor fixes to DatasetInstability.py

--- src/DatasetInstability.py
+++ src/DatasetInstability.py
@@ -25,18 +25,18 @@ k_fold = KFold(
 )
     
 # Arrays for metrics later (we need to save these)
-# 100 individual bootstrap indices
-
-
-# 10 validation indices
-validation_indices = []
-train_indices = []
-bootstrap_indices = []
-
+validation_indices = [] # 10 total validation indices
+train_indices = []      # 10 total train indices (oppositve to validation indices)
+bootstrap_indices = []  # 100 * 10 = 1000 total individual bootstrap indices
 # NOTE: No need to make test_indices since there's really only one official test X and y
 
 # init the dataset to train & test vars
 [X_train, y_train, X_test, y_test] = init_dataset()
+
+# NOTE: Since we only have one test for everything, we just scale & DMatricx test once
+scaler = StandardScaler()
+X_test_transf = scaler.transform(X_test)    
+xgb_test = xgb.DMatrix(X_test, y_test, enable_categorical=False)
 
 # Create the directory for saving the predictions
 predictions_dir = 'predictions/dataset_instability'
@@ -54,11 +54,10 @@ for fold_id, (train_idx, val_idx) in enumerate(k_fold.split(X_train)):
     X_val = X_train.iloc[val_idx]
     y_val = y_train[val_idx]
 
-    # bootstrapping for 10 rows la anay
     for b in range(1, bootstraps + 1): 
         print(f"BOOTSTRAP #{b}" )
         
-        # make bootstrapped sample of training dataset
+        # Make bootstrapped sample of training dataset
         n_samples = len(X_train_split)
         boot_idx = np.random.choice(
             n_samples, 
@@ -66,23 +65,22 @@ for fold_id, (train_idx, val_idx) in enumerate(k_fold.split(X_train)):
             replace=True
         )
         bootstrap_indices.append(boot_idx) # Saving the bootstrap indices
-        
+
+        # Create the bootstrapped training data
         X_boot = X_train_split.iloc[boot_idx]
         y_boot = y_train_split[boot_idx]
         
-        # Train RF and XGBoost models
+        # Train RF
         RF_Classifier(predictions_dir, X_boot, y_boot, X_val, y_val, X_test, y_test, fold_id, random_seed, b)# convert data for xgboost
         
-        xgb_train = xgb.DMatrix(X_boot, y_boot, enable_categorical=False)
+        # Create DMatrix of X_boot and X_val
+        xgb_boot = xgb.DMatrix(X_boot, y_boot, enable_categorical=False)
         xgb_val = xgb.DMatrix(X_val, y_val, enable_categorical=False)
-        xgb_test = xgb.DMatrix(X_test, y_test, enable_categorical=False)
-        XGBoost_Model(predictions_dir, xgb_train, y_boot, xgb_val, y_val, xgb_test, y_test, fold_id, random_seed, b)
+        XGBoost_Model(predictions_dir, xgb_boot, y_boot, xgb_val, y_val, xgb_test, y_test, fold_id, random_seed, b)
         
-        # fit the training data and transform the test data
-        scaler = StandardScaler()
+        # fit X_boot and transform the X_val 
         X_boot_scaled = scaler.fit_transform(X_boot)
         X_val_transf = scaler.transform(X_val)
-        X_test_transf = scaler.transform(X_test)
         
         # Train LR, MLP, and LinearSVC models
         LR_Model(predictions_dir, X_boot_scaled, y_boot, X_val_transf, y_val, X_test_transf, y_test, fold_id, random_seed, b)
