a8fc4fd5a81d40d667cacd5ff65d124eec2b9ad3
nikkanap <nenaputo@up.edu.ph>
2026-09-13 13:20:05 +0800

    Rewrite generate_rediction_csv

--- src/generate_prediction_csv.py
+++ src/generate_prediction_csv.py
@@ -1,24 +1,9 @@
 import pandas as pd
-import operator as op
-import os
 
 def generate_prediction_csv(
   y_pred_proba,
-  X_val_ids,
-  idx,
-  csv_file_path,
-  attribute_name
+  column_name,
+  csv_file_path
 ):
-  if not os.path.isfile(csv_file_path):
-    val_id_df = pd.DataFrame({
-      f'defendant_id': X_val_ids if op.contains(csv_file_path, 'Validation') else range(len(y_pred_proba))
-    })
-    val_id_df.to_csv(csv_file_path, index=False)
-    
-  predictions_df = pd.DataFrame({
-    f'{attribute_name}_{idx}' : y_pred_proba
-  })
-
-  predictions_df = pd.read_csv(csv_file_path)
-  predictions_df[f'{attribute_name}_{idx}'] = y_pred_proba
-  predictions_df.to_csv(csv_file_path, index=False)
+  df = pd.DataFrame({ column_name : y_pred_proba })
+  df.to_csv(csv_file_path, index=False)
